b9365f160a320aa58d76adb2bcc3d7bbbef811c5
Luigi Balotta <l.balotta@mindicity.com>
2025-12-04 12:37:53 +0100

    Bump version to v1.1.1

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -151,7 +151,7 @@ class MainWindow(QMainWindow):
             "About PC Assistant",
             "<h2>PC Assistant</h2>"
             "<p>Advanced System Cleaner & Optimizer</p>"
-            "<p>Version 1.0.1</p>"
+            "<p>Version 1.1.1</p>"
             "<p>Features:</p>"
             "<ul>"
             "<li>System cleaning</li>"
